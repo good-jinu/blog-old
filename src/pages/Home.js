0e995c66d78b2cce07d46943a08437316286edca
JN <gooday6983@gmail.com>
2021-08-06 17:53:28 +0900

    Update on Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import './Home.css';
+
+const Home = () => {
+  return (
+    <ul className="MainMenu">
+      <li>About me</li>
+      <li>Photos</li>
+      <li>More</li>
+    </ul>
+  );
+};
+
+export default Home;
